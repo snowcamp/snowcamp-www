9f97428ac20b373d4be70ce9b3458b317d1e9dab
Michel Barret <michel.barret@snowcamp.io>
2024-06-02 11:21:16 +0200

    feat: global rework
    - move from Astro3 to 5
    - update colors to improve contrast
    - use MDX

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,25 @@
 import { defineConfig } from 'astro/config';
-import { i18n } from "astro-i18n-aut/integration";
+import mdx from '@astrojs/mdx';
+import yaml from '@rollup/plugin-yaml';
 
-export const defaultLocale = "fr";
-export const locales = {
-  en: "en-US", // the `defaultLocale` value must present in `locales` keys
-  fr: "fr-FR",
-};
+const defaultLocale = 'fr';
+const locales = ['en', 'fr'];
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://snowcamp.io',
-  trailingSlash: "always",
-  build: { format: "directory" },
-  integrations: [
-    i18n({ locales, defaultLocale })
-  ]
+  build: {
+    format: 'directory'
+  },
+  i18n: {
+    defaultLocale,
+    locales,
+    routing: {
+      prefixDefaultLocale: false
+    }
+  },
+  integrations: [mdx()],
+  vite: {
+    plugins: [yaml()]
+  }
 });
