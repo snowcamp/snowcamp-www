0d51f85cddc3b17056b1d926c8c3c34e157c628c
Michel Barret <michel.barret@gmail.com>
2025-10-04 23:16:25 +0200

    chore: format files

--- src/data/create_previous_edition.js
+++ src/data/create_previous_edition.js
@@ -1,30 +1,41 @@
-import * as fs from 'fs';
+import * as fs from "fs";
 
 // This program collect data of the current conference edition and build a unique json file as previous_version
 
 // load data from the current file
-const edition = JSON.parse(fs.readFileSync('src/data/edition.json', 'utf8'));
-const cfp = JSON.parse(fs.readFileSync('src/data/cfp.json', 'utf8'));
-const sponsors = JSON.parse(fs.readFileSync('src/data/sponsors.json', 'utf8'));
+const edition = JSON.parse(fs.readFileSync("src/data/edition.json", "utf8"));
+const cfp = JSON.parse(fs.readFileSync("src/data/cfp.json", "utf8"));
+const sponsors = JSON.parse(fs.readFileSync("src/data/sponsors.json", "utf8"));
 
 // build the data for the previous edition
 const previous = {
-    sched_url: cfp.sched_url,
-    flickr_embed_url: "https://flickrembed.com/cms_embed.php?source=flickr&layout=responsive&input=www.flickr.com/photos/162459903@N02/albums/72177720305519001&sort=0&by=album&theme=default&scale=fill&speed=3000&limit=10&skin=default&autoplay=true",
-    punch_line: {
-      fr: "Une super conf IT a Grenoble",
-      en: "A top IT conference at Grenoble"
-    },
-    summary: {
-      fr: "Retrouvez le programme de SnowCamp %s <a href=\"%s\" target=\"_blank\">ici</a> ! Vous pouvez aussi retrouver les liens vers les slides des conférences dans <a href=\""+cfp.sched_url+"\">Sched</a>, et les retours des participants <a href=\""+cfp.feedback_url+"\">là</a>",
-      en: "The agenda for SnowCamp %s is available <a href=\"%s\" target=\"_blank\">here</a>! You can also find the links to the sessions material in <a href=\""+cfp.sched_url+"\">Sched</a>, and the participants feedback <a href=\""+cfp.feedback_url+"\">there</a>."
-    },
-    sponsors : {
-        etoile: sponsors.etoile,
-        flocon: sponsors.flocon
-    }
+  sched_url: cfp.sched_url,
+  flickr_embed_url:
+    "https://flickrembed.com/cms_embed.php?source=flickr&layout=responsive&input=www.flickr.com/photos/162459903@N02/albums/72177720305519001&sort=0&by=album&theme=default&scale=fill&speed=3000&limit=10&skin=default&autoplay=true",
+  punch_line: {
+    fr: "Une super conf IT a Grenoble",
+    en: "A top IT conference at Grenoble",
+  },
+  summary: {
+    fr:
+      'Retrouvez le programme de SnowCamp %s <a href="%s" target="_blank">ici</a> ! Vous pouvez aussi retrouver les liens vers les slides des conférences dans <a href="' +
+      cfp.sched_url +
+      '">Sched</a>, et les retours des participants <a href="' +
+      cfp.feedback_url +
+      '">là</a>',
+    en:
+      'The agenda for SnowCamp %s is available <a href="%s" target="_blank">here</a>! You can also find the links to the sessions material in <a href="' +
+      cfp.sched_url +
+      '">Sched</a>, and the participants feedback <a href="' +
+      cfp.feedback_url +
+      '">there</a>.',
+  },
+  sponsors: {
+    etoile: sponsors.etoile,
+    flocon: sponsors.flocon,
+  },
 };
 
 // create the file
-const fileName = 'src/data/previous_editions_'+edition.year+'.json';
-fs.writeFileSync(fileName, JSON.stringify(previous, null, 2), 'utf8');
+const fileName = "src/data/previous_editions_" + edition.year + ".json";
+fs.writeFileSync(fileName, JSON.stringify(previous, null, 2), "utf8");
--- voxxrin/talks-stats.cjs
+++ voxxrin/talks-stats.cjs
@@ -1,51 +1,53 @@
-const fs = require('fs');
-const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const fs = require("fs");
+const createCsvWriter = require("csv-writer").createObjectCsvWriter;
 
 // Fonction principale
 async function convertJsonToCsv(inputFilePath, outputFilePath) {
-    try {
-        // Lire le fichier JSON
-        const jsonData = JSON.parse(fs.readFileSync(inputFilePath, 'utf8'));
+  try {
+    // Lire le fichier JSON
+    const jsonData = JSON.parse(fs.readFileSync(inputFilePath, "utf8"));
 
-        // Vérification de la structure attendue
-        if (!jsonData.perTalkStats || !Array.isArray(jsonData.perTalkStats)) {
-            throw new Error("Le fichier JSON n'a pas le format attendu.");
-        }
-
-        // Créer un tableau contenant les données pour le CSV
-        const csvData = jsonData.perTalkStats.map(item => ({
-            talkId: item.talkId,
-            talkTitle: item.talkTitle,
-            totalFavoritesCount: item.totalFavoritesCount
-        }));
-
-        // Configurer le writer CSV
-        const csvWriter = createCsvWriter({
-            path: outputFilePath,
-            header: [
-                { id: 'talkId', title: 'talkId' },
-                { id: 'talkTitle', title: 'talkTitle' },
-                { id: 'totalFavoritesCount', title: 'totalFavoritesCount' }
-            ]
-        });
-
-        // Écrire les données dans le fichier CSV
-        await csvWriter.writeRecords(csvData);
-
-        console.log(`Fichier CSV généré avec succès : ${outputFilePath}`);
-    } catch (error) {
-        console.error(`Erreur : ${error.message}`);
+    // Vérification de la structure attendue
+    if (!jsonData.perTalkStats || !Array.isArray(jsonData.perTalkStats)) {
+      throw new Error("Le fichier JSON n'a pas le format attendu.");
     }
+
+    // Créer un tableau contenant les données pour le CSV
+    const csvData = jsonData.perTalkStats.map((item) => ({
+      talkId: item.talkId,
+      talkTitle: item.talkTitle,
+      totalFavoritesCount: item.totalFavoritesCount,
+    }));
+
+    // Configurer le writer CSV
+    const csvWriter = createCsvWriter({
+      path: outputFilePath,
+      header: [
+        { id: "talkId", title: "talkId" },
+        { id: "talkTitle", title: "talkTitle" },
+        { id: "totalFavoritesCount", title: "totalFavoritesCount" },
+      ],
+    });
+
+    // Écrire les données dans le fichier CSV
+    await csvWriter.writeRecords(csvData);
+
+    console.log(`Fichier CSV généré avec succès : ${outputFilePath}`);
+  } catch (error) {
+    console.error(`Erreur : ${error.message}`);
+  }
 }
 
 // Récupérer les paramètres de la ligne de commande
 const args = process.argv.slice(2);
 
 if (args.length < 1) {
-    console.error("Usage : node talks-stats.cjs <chemin_du_fichier_JSON> [<chemin_du_fichier_CSV>]");
-    process.exit(1);
+  console.error(
+    "Usage : node talks-stats.cjs <chemin_du_fichier_JSON> [<chemin_du_fichier_CSV>]",
+  );
+  process.exit(1);
 }
 
 const inputFilePath = args[0];
-const outputFilePath = args.length > 1 ? args[1] : 'talks-stats.csv';
+const outputFilePath = args.length > 1 ? args[1] : "talks-stats.csv";
 convertJsonToCsv(inputFilePath, outputFilePath);
